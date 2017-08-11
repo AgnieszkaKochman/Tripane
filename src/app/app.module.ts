d485e81f42a8438bc34845db2cfa62cba8183173
AgnieszkaKochman <kochman.aga@gmail.com>
2017-08-11 11:48:59 +0200

    header, photo and simple search templates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppComponent }  from './app.component';
+import { HeaderComponent }  from './header.component/header.component';
+import { SimpleSearchComponent }  from './simple-search.component/simple-search.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent,
+                  HeaderComponent,
+                  SimpleSearchComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
